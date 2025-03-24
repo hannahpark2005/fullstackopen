d8be5537e4a4120fa6c7244f39e16f5efcaf824d
Sunghyun Hannah Park <park2005hannah@gmail.com>
2025-03-24 19:46:06 +0900

    Complete 1.13

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -11,12 +11,20 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
+
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
 
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={() => {
+        const copy = [...votes]
+        copy[selected] += 1
+        setVotes(copy)
+      }
+      }>vote</button>
       <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>next anecdote</button>
     </div>
   )
