880aa59df5112e185594fcc602f84641f7f66c55
Sunghyun Hannah Park <park2005hannah@gmail.com>
2025-03-25 00:48:38 +0900

    Complete 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,33 +1,4 @@
-const Header = (props) => <h2>{props.course}</h2>
-
-const Content = (props) => (
-  <div>
-    {props.parts.map(part =>
-      <Part key={part.id} part={part} />
-    )}
-  </div>
-)
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
-
-const Total = (props) => <p><b>total of exercises {props.total}</b></p>
-
-const Course = ({ course }) => {
-
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total total={course.parts.reduce(
-        (acc, currVal) => acc + currVal.exercises, 0
-      )} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
