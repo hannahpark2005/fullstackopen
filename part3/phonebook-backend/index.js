a535c73cf6a17d4ba65d5d191d414909ceffd7c6
Sunghyun Hannah Park <park2005hannah@gmail.com>
2025-03-30 22:39:24 +0900

    Complete 3.10

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+// const cors = require('cors')
+
+// app.use(cors())
+
+app.use(express.static('dist'))
+
 var morgan = require('morgan')
 
 app.use(express.json())
@@ -103,7 +109,7 @@ app.post('/api/persons', (request, response) => {
 
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
